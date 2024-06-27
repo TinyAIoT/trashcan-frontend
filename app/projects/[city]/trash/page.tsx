10494beb17336808c3ffaa5c4ce6b3ad7e1e4f2b
Sven Ligensa <sven.ligensa@gmail.com>
2024-06-27 08:59:11 +0200

    Fixed link of map page to correct trashbin details page

--- app/projects/[city]/trash/page.tsx
+++ app/projects/[city]/trash/page.tsx
@@ -32,7 +32,6 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-5  w-full">
       <PageTitle title="Dashboard" />
-      <h2 className="text-2xl font-semibold">Distribution of fill level of all bins</h2>
       <section className="grid grid-cols-1  gap-4 transition-all lg:grid-cols-2">
         <CardContent>
           <p className="p-4 font-semibold">Distribution of fill level of all bins</p>
